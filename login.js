41137522bd9be789b8f81187b9444a790e9f9a3e
Ray <675983230@qq.com>
2024-04-28 22:03:34 +0800

    init

---
+++ login.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+    // Read the config file
+    const data = fs.readFileSync('config.json', 'utf8');
+    const config = JSON.parse(data);
+
+    const browser = await puppeteer.launch({
+        headless: config.headless,
+        userDataDir: './data',
+    });
+    const page = await browser.newPage();
+    await page.goto('http://222.204.3.154');
+
+    await page.type('#username', config.username);
+    await page.type('#password', config.password);
+    
+    // Set the value the hidden input field
+    await page.evaluate((domain) => {
+        document.querySelector('#domain').value = domain;
+    }, config.domain);
+
+    // Wait for navigation and click the login button at the same time
+    await Promise.all([
+        page.waitForNavigation({ waitUntil: 'networkidle0' }),
+        page.click('#login'), // assuming '#login' is the selector for the login button
+    ]);
+
+    await browser.close();
+})();
